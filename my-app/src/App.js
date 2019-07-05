c892f13f1a79eef25ac680212a8f8236c40016e4
ABDev <arkadiuszbiesiada@gmail.com>
2019-07-05 11:03:57 +0200

    Basic Project Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default './index.js';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>Hello World</div>
+        );
+    }
+}
