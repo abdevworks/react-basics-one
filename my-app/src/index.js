c32ef697c4fff93b7b0c99f26d693cc7c290107d
ABDev <arkadiuszbiesiada@gmail.com>
2019-07-05 11:15:47 +0200

    Part One finished.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-
 class App extends React.Component {
-    render() {
-        return(
-            <div>Hello World</div>
-        );
-    }
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
